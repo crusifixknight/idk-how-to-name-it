cc34f45681cefbba294e45d5936161adba3c40f9
crusifixknight <equinox.salieri@gmail.com>
2026-06-14 14:07:13 +0300

    feat: dynamic cast in AForEachUI (needs completion)

--- src/BookList.cpp
+++ src/BookList.cpp
@@ -1,4 +1,7 @@
 #include "BookList.h"
+
+#include "Models/EBook.h"
+
 #include <AUI/Util/Declarative/Containers.h>
 #include <AUI/View/AForEachUI.h>
 
@@ -6,10 +9,14 @@ using namespace declarative;
 
 
 BookList::BookList(AProperty<AVector<_<Book>>> Books): mBooks(std::move(Books)) {
-	setContents(Vertical{ 
-		AUI_DECLARATIVE_FOR(i, *mBooks, AVerticalLayout) {
-        return Label { "{}"_format(i) 
-		};
+	setContents(Vertical{
+	    AUI_DECLARATIVE_FOR(i, *mBooks, AVerticalLayout) {
+                const auto ebook = dynamic_pointer_cast<EBook>(i);
+	        if (ebook) {
+	            return Label { "Ebook: {}"_format(i->title())};
+	        }
+	        //TODO: Add for other books
+	        return Label{ "Other books" };
     }
 	}); 
 }
