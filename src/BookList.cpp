15d0db0cac45eb9d8b0baaee6f95c581907b2f5f
crusifixknight <equinox.salieri@gmail.com>
2026-06-14 18:36:46 +0300

    style: change style in BookList.cpp, add BookViewProvider.h factory

--- src/BookList.cpp
+++ src/BookList.cpp
@@ -1,6 +1,9 @@
 #include "BookList.h"
 
+#include "BookViewProvider.h"
+#include "Models/AudioBook.h"
 #include "Models/EBook.h"
+#include "Models/PrintedBook.h"
 
 #include <AUI/Util/Declarative/Containers.h>
 #include <AUI/View/AForEachUI.h>
@@ -16,12 +19,7 @@ BookList::BookList(AProperty<AVector<_<Book>>> Books): mBooks(std::move(Books)) 
     setContents(Vertical {
       _new<ATextField>(),
       AUI_DECLARATIVE_FOR(i, *mBooks, AVerticalLayout) {
-          const auto ebook = dynamic_pointer_cast<EBook>(i);
-          if (ebook) {
-              return Label { "Ebook: {}"_format(i->title()) };
-          }
-          // TODO: Add for other books
-          return Label { "Other books" };
+            return BookViewProvider::createCompactView(i);
       },
       SpacerExpanding(),
       Horizontal {
---
+++ src/BookViewProvider.h
@@ -0,0 +1,79 @@
+//
+// Created by crusifixknight on 6/14/2026.
+//
+
+#include "Models/AudioBook.h"
+#include "Models/Book.h"
+#include "Models/EBook.h"
+#include "Models/PrintedBook.h"
+
+#include <AUI/Util/Declarative/Containers.h>
+#include <chrono>
+#include <sstream>
+#include <iomanip>
+#include <AUI/View/AButton.h>
+
+using namespace declarative;
+using namespace ass;
+
+using Duration = std::chrono::duration<double>;
+class BookViewProvider {
+public:
+    static _<AView> createCompactView(const _<Book>& book) {
+        return Button {
+            .content = Vertical {
+                Label { "{}, {}"_format(book->title(), getFormatString(book)) }
+                AUI_OVERRIDE_STYLE{ FontSize{ 14_dp }, LineHeight { 3_dp } },
+            Label { getDetailsLine(book) }
+                AUI_OVERRIDE_STYLE{ FontSize{ 11_dp }, TextColor{ AColor::GRAY } }
+            } AUI_OVERRIDE_STYLE {
+                Expanding {}
+            },
+            .onClick = [book] {
+                ALogger::debug("clicked");
+            }
+        } AUI_OVERRIDE_STYLE {
+            BackgroundSolid { AColor::GRAY.lighter(0.7f) },
+            BorderRadius { 8_dp },
+            Margin { 3_dp },
+            Padding { 4_dp }
+        };
+    }
+
+private:
+    static AString getFormatString(const _<Book>& book) {
+        if (const auto ebook = std::dynamic_pointer_cast<EBook>(book)) {
+            return "EBook";
+        }
+        if (const auto audiobook = std::dynamic_pointer_cast<AudioBook>(book)) {
+            return "Audio Book, Duration: {}"_format(formatDuration(audiobook->duration()));
+        }
+        if (const auto printed = std::dynamic_pointer_cast<PrintedBook>(book)) {
+            return "Printed Book";
+        }
+        return "Unknown";
+    }
+
+    static AString getDetailsLine(const _<Book>& book) {
+        AString baseInfo = "Author: {}, Publisher: {}, Year: {}"_format(
+            book->author(), book->publisher(), book->year()
+        );
+
+        if (const auto printed = std::dynamic_pointer_cast<PrintedBook>(book)) {
+            return "{}, Pages: {}"_format(baseInfo, printed->countOfPages());
+        }
+        return baseInfo;
+    }
+
+    static AString formatDuration(const Duration duration) {
+        const auto hms = std::chrono::hh_mm_ss(duration);
+
+        std::ostringstream oss;
+        oss << std::setfill('0')
+            << std::setw(2) << hms.hours().count() << ":"
+            << std::setw(2) << hms.minutes().count() << ":"
+            << std::setw(2) << hms.seconds().count();
+
+        return oss.str();
+    }
+};
