19764c11f0fafac586b684b9d51ae91764957175
crusifixknight <equinox.salieri@gmail.com>
2026-06-14 17:28:47 +0300

    feat: add default data

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,33 @@
+#include "BookList.h"
+
 #include <AUI/Platform/Entry.h>
-#include "MainWindow.h"
 #include "MyUpdater.h"
-#include <BookList.h>
+#include "UserList.h"
+
 #include <Models/Library.h>
 #include <Models/EBook.h>
-#include <UserList.h>
 #include <BookAdvanced.h>
 #include <Models/AudioBook.h>
 #include <Models/PrintedBook.h>
 
 AUI_ENTRY {
+    auto readers = AVector<_<Reader>> {
+        _new<Reader>("Michael"),
+        _new<Reader>("John"),
+        _new<Reader>("Jane"),
+        _new<Reader>("Pablo")
+    };
+    auto books = AVector<_<Book>> {
+        _new<AudioBook>("1984", "George Orwell", "Secker & Warburg", 1949, std::chrono::duration<double>(3600.0)),
+        _new<AudioBook>("Brave New World", "Aldous Huxley", "Chatto & Windus", 1932, std::chrono::duration<double>(4500.0)),
+        _new<EBook>("The Great Gatsby", "F. Scott Fitzgerald", "Charles Scribner's Sons", 1925),
+        _new<EBook>("Moby Dick", "Herman Melville", "Harper & Brothers", 1851),
+        _new<PrintedBook>("War and Peace", "Leo Tolstoy", "The Russian Messenger", 1869, 1225),
+        _new<PrintedBook>("Crime and Punishment", "Fyodor Dostoevsky", "The Russian Messenger", 1866, 671)
+    };
 
-    auto library = _new<Library>();
-    library->addBook(_new<AudioBook>("1984", "George Orwell", "Beba", 1488, std::chrono::duration<double>(2.5)));
-    //_new<BookList>(library->books())->show();
-
-
-    //_new<UserList>(library->readers())->show();
-
-    auto a = library->books()->at(0);
-    _new<BookAdvanced>(a)->show();
+    auto library = _new<Library>(books, readers);
+    _new<UserList>(library->readers())->show();
+    _new<BookList>(library->books())->show();
     return 0;
 };
