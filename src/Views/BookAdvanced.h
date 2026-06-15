da35ab09760c4aa24cd4ba86ae26f55f8164ac0d
Shalopai <Shalopai@SHALOPAI>
2026-06-15 21:03:30 +0300

    Implemented Pluey

--- src/Views/BookAdvanced.h
+++ src/Views/BookAdvanced.h
@@ -6,12 +6,14 @@
 
 #include <AUI/Platform/AWindow.h>
 #include <AUI/View/AButton.h>
+#include <AUI/View/ATextField.h>
 
 using namespace declarative;
 class BookAdvanced : public AWindow {
 public:
     BookAdvanced(_<Book>);
     _<AView> Huyni() const noexcept;
+    _<AView> HuyniEdit() noexcept;
 
     ~BookAdvanced() override = default;
     
@@ -19,12 +21,23 @@ private:
     _<Book> mBook;
     _<AButton> mButton;
 
+    _<ATextField> tfName;
+    _<ATextField> tfAuthor;
+    _<ATextField> tfYear;
+    _<ATextField> tfPublisher;
+
+    _<ATextField> tfCountOfPages;
+    _<ATextField> tfDuration;
+
     struct State {
         AProperty<_<Reader>> mSelectedReader = nullptr;
         AProperty<AVector<_<Reader>>> mReaders;
     };
     void ParamList();
     void ParamListEdit();
+
+    void SaveEdit();
+
 
     void buttonControl(_<Reader> const&) noexcept;
 };
